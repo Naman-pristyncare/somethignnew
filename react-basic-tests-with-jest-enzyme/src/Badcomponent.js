0128c40bbdecfe24d138435b0ede23a15710e833
naman-pristyncare <naman.ahuja@pristyncare.com>
2021-07-30 16:25:55 +0530

    minin

---
+++ react-basic-tests-with-jest-enzyme/src/Badcomponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Badcomponent({heroName}) {
+if(heroName=="joker")
+throw new Error("not a hero")
+    // const [name,setName]=useState("");
+    // var arr;
+    return (
+        <div>
+            
+            {heroName}
+
+        </div>
+    )
+}
+
+export default Badcomponent
